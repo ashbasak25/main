b9d51de7662a0373720de160815c411c53b4a38f
Aslihan Başak <aslihanbasak@Aslihans-MacBook-Pro.local>
2025-07-22 12:01:40 -0400

    initial clean commit on main

---
+++ steps/getting-started/startAndRefundDates.js
@@ -0,0 +1,29 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect} from "@playwright/test";
+import { startApplicationPage, page } from "../../globalPagesSetup.js";
+import { productInfo } from "../../utilities/qa-data-reader.js";
+       
+         Given('user is on the enrollment page', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+        
+         Then('the program start date is displayed', async function () {
+        });
+        
+        Then('the program refund date is displayed', async function () {
+        });
+     
+       
+         Then('the program start date for the program is correct', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+       
+         Then('the program refund date for the program is correct', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+        
